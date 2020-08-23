ee021f99e94335e545c6af17842f89d1b8f723db
Shreyans Jain <jainshreyans121@gmail.com>
2020-08-24 00:03:58 +0530

    Updates

---
+++ precache-manifest.7c78e5db4c3ca37bc6104dde2731a51c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eda0277f22fe37d18b2963b3f41b2e0",
+    "url": "/React-app/index.html"
+  },
+  {
+    "revision": "ac95c8ae327f17ea8b88",
+    "url": "/React-app/static/css/main.e3628d90.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/React-app/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-app/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac95c8ae327f17ea8b88",
+    "url": "/React-app/static/js/main.6dc337f1.chunk.js"
+  },
+  {
+    "revision": "3e54e0a670310f4601a0",
+    "url": "/React-app/static/js/runtime-main.28337175.js"
+  }
+]);
